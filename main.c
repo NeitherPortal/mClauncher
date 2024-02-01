2f16778d86ec70bfd60d4ecf57534b6fcb80eac7
hu <18522604634@163.com>
2024-02-01 15:24:08 +0800

    Changes

--- main.c
+++ main.c
@@ -2,96 +2,22 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-// 如果使用GUI
-#ifdef USE_GUI
-#include <gtk/gtk.h>
-// 或者使用Qt
-// #include <QtWidgets>
-#endif
+#include <gtk-4.0/gtk/gtk.h>
 
-// 如果使用TUI
-#ifdef USE_TUI
 #include <ncurses.h>
-#endif
 
-// 如果使用Lua
-#ifdef USE_LUA
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
-#endif
 
-// 如果使用JSON解析
-#ifdef USE_JSON
 #include <json-c/json.h>
-#endif
 
 // 其他可能用到的头文件
 
-// 函数声明
-void* thread_function(void* arg);
-
-int main() {
-    // 初始化GUI
-    #ifdef USE_GUI
-    gtk_init(NULL, NULL);
-    // 或者初始化Qt
-    // ...
-    #endif
-
-    // 初始化TUI
-    #ifdef USE_TUI
+//tui部分代码
+int main(){
     initscr();
-    // ...
 
-    // 启动TUI主循环
-    // ...
-    #endif
-
-    // 初始化Lua
-    #ifdef USE_LUA
-    lua_State* L = luaL_newstate();
-    luaL_openlibs(L);
-    // ...
-
-    // 执行Lua脚本
-    // ...
-    #endif
-
-    // 创建线程
-    pthread_t thread;
-    pthread_create(&thread, NULL, thread_function, NULL);
-
-    // 主线程执行其他任务
-    // ...
-
-    // 等待线程结束
-    pthread_join(thread, NULL);
-
-    // 清理资源
-    #ifdef USE_GUI
-    gtk_main_quit();
-    // 或者清理Qt资源
-    // ...
-    #endif
-
-    #ifdef USE_TUI
     endwin();
-    // ...
-    #endif
-
-    #ifdef USE_LUA
-    lua_close(L);
-    // ...
-    #endif
-
     return 0;
-}
-
-// 示例线程函数
-void* thread_function(void* arg) {
-    // 线程执行的任务
-    // ...
-
-    pthread_exit(NULL);
 }
